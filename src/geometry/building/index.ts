269e705be14331166a25d3bb319c5e7bdf8a4ea0
prms-magnet <prmsmagnet@protonmail.com>
2026-05-13 09:55:52 -0700

    feat(building): Public API surface in index.ts

--- src/geometry/building/index.ts
+++ src/geometry/building/index.ts
@@ -1,10 +1,25 @@
 export { buildingCutlist } from './compose';
-export { simpleGablePreset } from './presets';
+export {
+  simpleGablePreset,
+  lPlanPreset,
+  tPlanPreset,
+  BUILTIN_BUILDING_PRESETS,
+} from './presets';
+export { buildPlanDiagram } from './diagram';
+export { unitPlacement, applyPlacementToPiece3D } from './place3d';
+export { computeIntersection } from './intersect';
+
 export type {
   Building,
   Intersection,
   IntersectionKind,
+  IntersectionPlacement,
+  CrossGableTPlacement,
+  CrossGableLPlacement,
   BuildingOutput,
   BuildingDerived,
   IntersectionDerived,
+  ComputeIntersectionResult,
 } from './types';
+export type { BuildingPreset, UnitWithSheet } from './presets';
+export type { UnitPlacement } from './place3d';
